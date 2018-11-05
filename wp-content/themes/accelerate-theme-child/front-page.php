10d5ede4eb931663efb200edc3caa95e4239f61b
Elena Greco <elenawgreco@gmail.com>
2018-11-04 19:43:41 -0600

    add featured work on homepage

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -23,16 +23,41 @@ get_header(); ?>
 		</div><!-- .main-content -->
 	</div><!-- #primary -->
 
+<section class="featured-work">
+	<div class="site-content">
+		<h4>Featured Work</h4>
+		<ul class="homepage-featured-work">
+			<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
+						<?php while ( have_posts() ) : the_post(); 
+							$image_1 = get_field('image_1');
+							$size = 'small';
+						?>
+						<li class="individual-featured-work">
+							<figure>
+								<?php echo wp_get_attachment_image( $image_1, $size ); ?>
+							</figure>
+
+							<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+						</li>
+						
+						<?php endwhile; // end of the loop ?> 
+    	<?php wp_reset_query(); // resets the alterd query back to the original ?>
+		</ul>	
+
+	</div>
+</section>
+
 <section class="recent-posts">
   <div class="site-content">
-	<div class="blogpost"></div>
-		<h4>From the Blog</h4>
-			<?php query_posts('posts_per_page=1'); ?>
-				<?php while ( have_posts() ) : the_post(); ?>
-       		<h3><?php the_title(); ?></h3>
-       		<?php the_excerpt(); ?> 
-     		<?php endwhile; ?> 
-    	<?php wp_reset_query(); ?>
+		<div class="blogpost">
+			<h4>From the Blog</h4>
+				<?php query_posts('posts_per_page=1'); ?>
+					<?php while ( have_posts() ) : the_post(); ?>
+						<h3><?php the_title(); ?></h3>
+						<?php the_excerpt(); ?> 
+					<?php endwhile; ?> 
+				<?php wp_reset_query(); ?>
+		</div>
  </div>
 </section>
 <?php get_footer(); ?>
